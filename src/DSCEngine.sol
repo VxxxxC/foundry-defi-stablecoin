da826214b5c2b254d430049017ee04f0d5b4f041
vxxxxc <veper1234@icloud.com>
2025-11-10 15:42:20 +0800

    [WIP] create DSCEngine contract

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.20;
 
+import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+
 /**
  * @title DSCEngine
  * @author VxxxxC
@@ -19,11 +21,76 @@ pragma solidity ^0.8.20;
  * @notice This is contract is VERY loosely based on the MakerDAO DSS (DAI Stablecoin System).
  */
 contract DSCEngine {
+    /////////////////////
+    // State Variables //
+    /////////////////////
+    mapping(address token => address priceFeed) private s_priceFeeds; // Token to price feed
+    DecentralizedStableCoin private immutable i_dsc;
+
+    /////////////////////
+    //      Errors     //
+    /////////////////////
+    error DSCEngine__AmountMustBeMoreThanZero();
+    error DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+    error DSCEngine__NotAllowedToken();
+
+    /////////////////////
+    //    Modifiers    //
+    /////////////////////
+
+    modifier moreThanZero(uint256 amount) {
+        if (amount == 0) {
+            revert DSCEngine__AmountMustBeMoreThanZero();
+        }
+        _;
+    }
+
+    modifier isAollowedToken(address token) {
+        if (s_priceFeeds[token] == address(0)) {
+            revert DSCEngine__NotAllowedToken();
+        }
+        _;
+    }
+
+    /////////////////////
+    //    Functions    //
+    /////////////////////
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address dscAddress) {
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+        }
+
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+        }
+        i_dsc = DecentralizedStableCoin(dscAddress);
+    }
+
+    ////////////////////////
+    // external Functions //
+    ///////////////////////
     function depositCollateralAndMintDsc() external {}
 
-    function redeemCollateralForDex() external {}
+    /**
+     * @param tokenCollateralAddress The address of the token to deposit as collateral
+     * @param amountCollateral The amount of collateral to deposit
+     *
+     */
+    function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral)
+        external
+        moreThanZero(amountCollateral)
+        isAollowedToken(tokenCollateralAddress)
+    {}
+
+    function redeemCollateralForDsc() external {}
+
+    function redeemCollateral() external {}
+
+    function mintDes() external {}
 
     function burnDsc() external {}
 
     function liquidate() external {}
+
+    function getHealthFactor() external {}
 }
